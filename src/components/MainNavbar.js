d3e6c138dba70f493dc560d51d908a79b9dbe40b
Wuletaw Wonte <wuletawwonte@gmail.com>
2021-11-05 11:46:46 +0300

    layouts folder added

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,18 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 import './mainNavbar.css';
-import {red} from '@mui/material/colors';
 
 const MainNavbar = (props) => {
-  const color = red;
   const navigate = useNavigate();
   return <AppBar
     {...props}
     position="sticky"
     color="primary"
   >
-    <Toolbar variant="dense">
-      <Typography className="navbarTitle" variant="h6" component="div" onClick={() => {navigate('/');}}>
+    <Toolbar variant="regular">
+      <Typography className="navbarTitle" variant="h4" component="div" onClick={() => {navigate('/');}}>
         ChurchDb
       </Typography>
       <Button color="inherit" onClick={() => {navigate('/login')}}>Login</Button>
